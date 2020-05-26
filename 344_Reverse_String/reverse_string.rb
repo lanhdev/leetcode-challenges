4de5b860ea82e0652a094b71d4199ff4d3f65430
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-05-26 21:46:24 +0700

    LeetCode: Add recursive solution for Reverse String

--- 344_Reverse_String/reverse_string.rb
+++ 344_Reverse_String/reverse_string.rb
@@ -3,20 +3,35 @@
 # @param {String} s
 # @return {String}
 
-# Two Pointers
 def reverse_string(s)
-  left = 0
-  right = s.length - 1
-  while left < right
-    s[left], s[right] = s[right], s[left]
-    left += 1
-    right -= 1
-  end
-  
-  s
+  Solution.two_pointers(s)
+  Solution.recursive(s, 0, s.length - 1)
+  Solution.ruby_reverse(s)
 end
 
-# Ruby built-in
-def reverse_string(s)
-  s.reverse
+module Solution
+  # Two Pointers
+  def self.two_pointers(str)
+    left = 0
+    right = str.length - 1
+    while left < right
+      str[left], str[right] = str[right], str[left]
+      left += 1
+      right -= 1
+    end
+
+    str
+  end
+
+  # Recursive
+  def self.recursive(str, left, right)
+    return if left > right
+    str[left], str[right] = str[right], str[left]
+    recursive(str, left + 1, right - 1)
+  end
+
+  # Ruby built-in
+  def self.ruby_reverse(str)
+    str.reverse
+  end
 end
